2a639a7667ee9cdf2064cf1d2c70cb5d6f55a4c2
katwithaC <catherinerosearredondo@gmail.com>
2020-09-26 16:43:41 -0500

    Fixing localhost

---
+++ server.js
@@ -0,0 +1,36 @@
+var express = require("express");
+
+var app = express();
+
+var PORT = process.env.PORT || 3001;
+
+app.listen(PORT, function() {
+
+})
+
+app.get("/", function (req, res) {
+    res.json(path.join(__dirname, "public/index.html"));
+});
+
+
+
+app.get('/notes', function (req, res) }
+res.send('GET request to the homepage')
+)
+
+app.get('*', function (req, res) {
+    res.send('GET request to the homepage')
+})
+
+app.get('/api/notes', function (req,res) {
+    return res.json('GET all saved notes on JSON')
+})
+
+app.post('/api.notes', function (req, res) {
+    res.send('POST new note')
+})
+
+app.delete('/api/notes/:id', (req, res, next) => {
+    const notes = getNotesByID(req.params.id);
+    res.send(notes);
+});
