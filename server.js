a3bed8d8342be95b2da36cdc25a818995c2c70c3
katwithaC <catherinerosearredondo@gmail.com>
2020-12-15 08:55:57 -0600

    fixing server.js

--- server.js
+++ server.js
@@ -1,36 +1,20 @@
 var express = require("express");
 
+
 var app = express();
 
-var PORT = process.env.PORT || 3001;
+
+var PORT = process.env.PORT || 8080;
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
 
 app.listen(PORT, function() {
-
-})
-
-app.get("/", function (req, res) {
-    res.json(path.join(__dirname, "public/index.html"));
-});
-
-
-
-app.get('/notes', function (req, res) {
-    res.send('GET request to the homepage')
-})
-
-app.get('*', function (req, res) {
-    res.send('GET request to the homepage')
-})
-
-app.get('/api/notes', function (req,res) {
-    return res.json('GET all saved notes on JSON')
-})
-
-app.post('/api.notes', function (req, res) {
-    res.send('POST new note')
-})
-
-app.delete('/api/notes/:id', (req, res, next) => {
-    const notes = getNotesByID(req.params.id);
-    res.send(notes);
+  console.log("App listening on PORT: " + PORT);
 });
