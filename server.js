83d5396552acd029ea21945dd8a3f0ed39a82e37
katwithaC <catherinerosearredondo@gmail.com>
2020-12-15 19:32:03 -0600

    setting up heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3001;
 
 
 app.use(express.urlencoded({ extended: true }));
